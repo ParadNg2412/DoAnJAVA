71d3f7ab00001c7da7c147c25fb740daed2e268f
Parad Long Vu <90337365+ParadNg2412@users.noreply.github.com>
2022-04-10 23:05:57 +0700

    Add files via upload

---
+++ DAJava-Music.sql
@@ -0,0 +1,47 @@
+﻿create database DAJava
+use DAJava
+set dateformat DMY
+
+--Tạo table
+create table Account
+(
+	username varchar(20) primary key not null,
+	password varchar(8)
+)
+
+create table Singer
+(
+	MaCS varchar(5) primary key not null,
+	TenCS nvarchar(50) not null,
+	Descriptions nvarchar(100)
+)
+
+create table categories
+(
+	MaTL varchar(5) primary key not null,
+	TenTL nvarchar(50) not null
+)
+
+create table Music
+(
+	MaBH varchar(5) not null,
+	TenBH nvarchar(20) not null,
+	Uploaddate date,
+	MaCS varchar(5),
+	MaTL varchar(5),
+	primary key (MaBH, MaCS),
+	foreign key (MaCS) references Singer(MaCS),
+	foreign key (MaTL) references Categories(MaTL)
+)
+
+create table Playlist
+(
+	MaPL varchar(5) not null,
+	TenPL nvarchar(50) not null,
+	MaBH varchar(5),
+	primary key (MaPL, MaBH),
+	/*foreign key (MaBH) references Music(MaBH),
+	on update cascade,
+	on delete cascade*/
+)
+
